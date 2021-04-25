ee8a3d8b44b4dde956ae7f06963b9ce3c3e4fde7
paraglande03 <paraglande03@gmail.com>
2021-04-25 19:36:24 +0530

    Added : Lombok Dependancy to remove getters and setters and to get data through annotation '@DATA'

--- src/main/java/com/AddressBookProject/dto/ResponseDTO.java
+++ src/main/java/com/AddressBookProject/dto/ResponseDTO.java
@@ -1,20 +1,22 @@
 package com.AddressBookProject.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public  class ResponseDTO {
 	private String message;
 	private Object data;
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public Object getData() {
-		return data;
-	}
-	public void setData(Object data) {
-		this.data = data;
-	}
+//	public String getMessage() {
+//		return message;
+//	}
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//	public Object getData() {
+//		return data;
+//	}
+//	public void setData(Object data) {
+//		this.data = data;
+//	}
 	
 	public ResponseDTO(String message, Object data) {
 		super();
