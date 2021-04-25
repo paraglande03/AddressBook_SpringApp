b54c9c8b9bd7d0e4588d7575b41207008e7e89be
paraglande03 <paraglande03@gmail.com>
2021-04-25 13:28:16 +0530

    Added : Controller function to get data

--- src/main/java/com/AddressBookProject/controller/AddressBookController.java
+++ src/main/java/com/AddressBookProject/controller/AddressBookController.java
@@ -1,10 +1,18 @@
 package com.AddressBookProject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/addressbookservice")
 public class AddressBookController {
+	
+	@RequestMapping({"/get","","/"})
+	
+	public ResponseEntity<String> getAddressBookData(){
+		return new ResponseEntity<String>("Get call successful for data",HttpStatus.OK);
+	}
 
 }
